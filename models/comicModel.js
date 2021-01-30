16d01f568d42a4fc25b15697ca830227a68005c6
SamKlep <shamdemann23@gmail.com>
2021-01-30 05:18:56 -0600

    comicModel initiated.

---
+++ models/comicModel.js
@@ -0,0 +1,63 @@
+// Title, Sub-title, Publisher, Issue, Year, Author, Artist, Character, Condition, Description, Image
+
+import mongoose from 'mongoose'
+
+const comicSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+    publisher: {
+      type: String,
+      required: true,
+    },
+    issue: {
+      type: Number,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    artist: {
+      type: String,
+      required: true,
+    },
+    character: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Comic = mongoose.model('Comic', comicSchema)
+
+export default Comic
